59392fca0284115e1ff8dacc5975503225282736
DwarfGiant26 <albertusalvinjanitra@gmail.com>
2022-06-18 22:32:43 +1000

    bfs get files

--- DependencyGraph/node.py
+++ DependencyGraph/node.py
@@ -1,10 +1,36 @@
+import ast
+import importlib
+
 class FileNode():
     def __init__(self,name) -> None:
         self.name = name
-        self.imported_files = []
-        self.script = ""
+        self.imported_filenames = []
         self.classes = []
         self.functions = []
+        self.dependency_file_nodes = []
+
+        # get script
+        with open(name) as f:
+            self.script = f.read()
+            self.ast_tree = ast.parse(self.script)
+
+        # get imports, functions, and classes
+        for elem in self.ast_tree.body:
+            if type(elem) == ast.Import:
+                # get module path
+                import_name = elem.names[0].name
+                module_specs = importlib.util.find_spec(import_name)
+                module_path = module_specs.origin
+                # create node for imported file
+                child_file_node = FileNode(module_path)
+                self.dependency_file_nodes.append(child_file_node)
+            if type(elem) == ast.ClassDef:
+                class_name = elem.name
+                self.classes.append(class_name)
+            if type(elem) == ast.FunctionDef:
+                function_name = elem.name
+                self.functions.append(function_name)
+        
 
 class ClassNode():
     def __init__(self,name) -> None:
--- DependencyGraph/parser.py
+++ DependencyGraph/parser.py
@@ -6,7 +6,6 @@ Parse a python file into list of classes, functions
 """
 
 class ParsedFile():
-    def __init__(self,file) -> None:
+    def __init__(self,filename) -> None:
         with open(filename) as f:
-            
             self.ast_tree = ast.parse()
