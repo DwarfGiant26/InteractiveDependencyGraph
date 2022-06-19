48bcfb30188ce2a9e28762cf661e8509d1b96b25
albertusalvinjanitra@gmail.com <albertusalvinjanitra@gmail.com>
2022-06-19 21:21:53 +1000

    simpleHttpServer

--- DependencyGraph/__main__.py
+++ DependencyGraph/__main__.py
@@ -3,49 +3,20 @@
 
 
 import ast
+from http.server import HTTPServer,BaseHTTPRequestHandler
 
-trees = ast.parse("""
-import math
-import numpy
+class ReqHandler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200)
+    self.end_headers()
+    self.wfile.write("test".encode("utf-8"))
 
-class FunctionNode():
-    def __init__(self,name) -> None:
-        self.name = name
-        self.called_functions = []
-        self.line_no = 0
+def run(server_class=HTTPServer, handler_class=ReqHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
 
-@something
-def my_function(fname, lname):
-  print(fname + " " + lname)
-  print(fname)
-
-def my_function1234123(fname, lname):
-  print(fname + " " + lname)
-  print(fname)
-""")
-from io import StringIO
-from contextlib import redirect_stdout
-
-
-import importlib
-# implib = importlib.abc.ExecutionLoader()
-for elem in trees.body:
-    # if type(elem) == ast.Import:
-    #     module_specs = importlib.util.find_spec(elem.names[0].name)
-    #     module_path = module_specs.origin
-    #     print(module_path)
-    # print(ast.dump(elem))
-    if type(elem) == ast.FunctionDef:
-        print(elem.name)
-    # exec(f"import {elem.names[0].name}")
-    # f = StringIO()
-    # with redirect_stdout(f):
-    #     exec(f"print({elem.names[0].name}.__name__)")
-    # s = f.getvalue()
-    # print(s)
-
-
-
+run()
 
 # Call Create graph
 # create list of dependencies for mermaid -> [[<source>,<dest>,(<source_type>,<dest_type>)],...] 
